0b8dd23717f6e9ffef18a86d9ff74ea7226820db
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-11-08 00:02:09 -0500

    Switched out flickity app

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,7 +4,6 @@ import Portfolio1 from "../../assets/Modals/coding.png";
 import Portfolio2 from "../../assets/Modals/writing.png";
 import Portfolio3 from "../../assets/Modals/graphic-design.png";
 import Portfolio4 from "../../assets/Modals/animation.gif";
-import Slider from "./Slider";
 import "./Portfolio.css";
 import CodingModal from "../Modals/CodingModal";
 import WritingModal from "../Modals/WritingModal";
@@ -12,6 +11,7 @@ import GraphicModal from "../Modals/GraphicModal";
 import VideoModal from "../Modals/VideoModal";
 import { fadeInLeft } from "react-animations";
 import Radium, { StyleRoot } from "radium";
+import Flickity from "react-flickity-component";
 
 class Portfolio extends Component {
   render() {
@@ -49,46 +49,52 @@ class Portfolio extends Component {
       },
     };
 
+
     return (
       <StyleRoot>
-      <div style={styles.fadeInLeft} className="portfolio">
-        <Container className="portfolio-container">
-          <div className="carousel-wrapper">
-            <h2 className="portfolio-h2">Portfolio</h2>
-            <Slider
-              options={{
-                // autoPlay: 4000,
-                pauseAutoPlayOnHover: true,
-                wrapAround: false,
-                fullscreen: true,
-                adaptiveHeight: true,
-              }}
-            >
-              {portfolioItems.map(({ title, image, id, Modal }) => (
-                <div
-                  style={{
-                    width: "22em",
-                    height: "400px",
-                    margin: "3em 2em 0 2em",
-                    textAlign: "center",
-                  }}
-                  key={id}
-                >
-                  <h4>{title}</h4>
-                  <img
-                    style={{ height: "200px", margin: "1em" }}
-                    src={image}
-                    alt=""
-                  />
-                  {/* Where the magic happens */}
-                  {Modal && <Modal />}
-                </div>
-              ))}
-            </Slider>
-          </div>
-        </Container>
-      </div>
-      </ StyleRoot>
+        <div style={styles.fadeInLeft} className="portfolio">
+          <Container className="portfolio-container">
+            <div className="carousel-wrapper">
+              <h2 className="portfolio-h2">Portfolio</h2>
+              <Flickity
+                options={{
+                  // autoPlay: 4000,
+                  pauseAutoPlayOnHover: true,
+                  wrapAround: false,
+                  fullscreen: true,
+                  adaptiveHeight: true,
+                }}
+                className={"carousel"}
+                elementType={"div"} // default 'div'
+                disableImagesLoaded={false} // default false
+                reloadOnUpdate // default false
+                static // default false
+              >
+                {portfolioItems.map(({ title, image, id, Modal }) => (
+                  <div
+                    style={{
+                      width: "22em",
+                      height: "400px",
+                      margin: "3em 2em 0 2em",
+                      textAlign: "center",
+                    }}
+                    key={id}
+                  >
+                    <h4>{title}</h4>
+                    <img
+                      style={{ height: "200px", margin: "1em" }}
+                      src={image}
+                      alt=""
+                    />
+                    {/* Where the magic happens */}
+                    {Modal && <Modal />}
+                  </div>
+                ))}
+              </Flickity>
+            </div>
+          </Container>
+        </div>
+      </StyleRoot>
     );
   }
 }
