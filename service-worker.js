17a6ec65dc745b3fa292363c08c9f153f9fd0f89
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-11-12 21:45:53 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f627941f62b9af18b2de2fe8ddbab8c7.js"
+  "/precache-manifest.170077f899e3c5cc8bc6bc52f88259c2.js"
 );
 
 self.addEventListener('message', (event) => {
